53ecb8f5df13fcf9eeb161dc5987bb99ff4cda7a
Sabatino BIFARELLA <sabatino.tech@gmail.com>
2025-03-01 17:52:43 +0100

    Ajout de fonctionnalités pour gérer les animaux
    Implémentation d'une fonctionnalité pour lister les informations sur les animaux existants et permettre l'ajout de nouveaux animaux.
    Le cas "1" affiche maintenant les détails des animaux, tandis que le cas "2" permet à l'utilisateur d'entrer des informations sur un nouvel animal après avoir vérifié le nombre d'animaux déjà présents.
    Des validations supplémentaires ont été ajoutées pour garantir la validité des entrées et respecter les limites du nombre d'animaux.

--- GuidedProject/Starter/Program.cs
+++ GuidedProject/Starter/Program.cs
@@ -108,16 +108,88 @@ do
     {
         case "1":
             // List all of our current pet information
-            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < 6; j++)
+                    {
+                        Console.WriteLine(ourAnimals[i, j]);
+                    }
+                }
+            }
             Console.WriteLine("Press the Enter key to continue.");
             readResult = Console.ReadLine();
             break;
 
         case "2":
             // Add a new animal friend to the ourAnimals array
-            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
-            Console.WriteLine("Press the Enter key to continue.");
-            readResult = Console.ReadLine();
+            string anotherPet = "y";
+            int petCount = 0;
+            for (int i = 0; i < maxPets; i++)
+            {
+                if (ourAnimals[i, 0] != "ID #: ")
+                {
+                    petCount += 1;
+                }
+
+            }
+
+            if (petCount < maxPets)
+            {
+                Console.WriteLine($"We currently have {petCount} pets that need homes. We can manage {(maxPets - petCount)} more.");
+            }
+            bool validEntry = false;
+            // get species (cat or dog) - string animalSpecies is a required field 
+            do
+            {
+                Console.WriteLine("\n\rEnter 'dog' or 'cat' to begin a new entry");
+                readResult = Console.ReadLine();
+                if (readResult != null)
+                {
+                    animalSpecies = readResult.ToLower();
+                    if (animalSpecies != "dog" && animalSpecies != "cat")
+                    {
+                        //Console.WriteLine($"You entered: {animalSpecies}.");
+                        validEntry = false;
+                    }
+                    else
+                    {
+                        validEntry = true;
+                    }
+                }
+            } while (validEntry == false);
+
+            while (anotherPet == "y" && petCount < maxPets)
+            {
+                // increment petCount (the array is zero-based, so we increment the counter after adding to the array)
+                petCount = petCount + 1;
+
+                // check maxPet limit
+                if (petCount < maxPets)
+                {
+                    // another pet?
+                    Console.WriteLine("Do you want to enter info for another pet (y/n)");
+                    do
+                    {
+                        readResult = Console.ReadLine();
+                        if (readResult != null)
+                        {
+                            anotherPet = readResult.ToLower();
+                        }
+
+                    } while (anotherPet != "y" && anotherPet != "n");
+                }
+            }
+
+            if (petCount >= maxPets)
+            {
+                Console.WriteLine("We have reached our limit on the number of pets that we can manage.");
+                Console.WriteLine("Press the Enter key to continue.");
+                readResult = Console.ReadLine();
+            }
+
             break;
 
         case "3":
